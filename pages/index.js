5d6ee8c28f2ae81fee1d4dc06b5a37b8de4bda3e
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-03-28 19:21:13 -0500

    Move nav menu to Nav component

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,16 @@
 import Head from 'next/head';
-import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-      <ul>
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/photos">
-            <a>Photos</a>
-          </Link>
-        </li>
-      </ul>
       <Head>
         <title>My Blog</title>
         <meta
           name="My blog is powered by Next.js"
           content="My blog is powered by Next.js"
         />
-        <h1>Welcome to My Blog</h1>
       </Head>
+      <h1>Welcome to My Blog</h1>
     </div>
   );
 }
